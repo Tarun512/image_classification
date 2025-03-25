2897315097ae564e41ea04ce72651cf7bb18107c
Tarun512 <jt0149131@gmail.com>
2025-03-25 13:26:20 +0530

    commit

--- backend/main.py
+++ backend/main.py
@@ -78,4 +78,5 @@ def home():
 
 # ✅ Run FastAPI Server
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
